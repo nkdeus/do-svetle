ecae613f34744530d45cc8921eea1fae7ada1b01
nkdeus <nkdeus@gmail.com>
2023-03-18 22:31:28 -0500

    up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
-<h1 class="aaa">Welcome to SvelteKit</h1>
+<h1 class="">Welcome to ooooooooooooooooooo</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
